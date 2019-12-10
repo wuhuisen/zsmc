d7ddffaf0fb2e2f2c6a60b217714260f19969a60
unknown <unknown@example.com>
2019-12-10 09:44:17 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+//app.js
+App({
+  onLaunch: function () {
+    let that = this;
+    let timer = false;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.globalData.platform = res.platform
+        let totalTopHeight = 68
+        if (res.model.indexOf('iPhone X') !== -1) {
+          totalTopHeight = 88
+        } else if (res.model.indexOf('iPhone') !== -1) {
+          totalTopHeight = 64
+        }
+        that.globalData.statusBarHeight = res.statusBarHeight
+        that.globalData.titleBarHeight = totalTopHeight - res.statusBarHeight
+      },
+      failure() {
+        that.globalData.statusBarHeight = 0
+        that.globalData.titleBarHeight = 0
+      }
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,17 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    title: '中数美创',
+  },
+  //事件处理函数
+  
+  onLoad: function () {
+
+  },
+  getUserInfo: function(e) {
+   
+  }
+})
